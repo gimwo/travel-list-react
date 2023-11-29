f120e1cb03bb1c3356739b187d3828fb8532bf35
mig.alviar45 <mig.alviar45@gmail.com>
2023-11-29 09:29:47 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackageList from "./PackageList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleClearItems() {
+    const confirmed = window.confirm("Are you sure you want to clear?");
+
+    if (confirmed) setItems([]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackageList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
